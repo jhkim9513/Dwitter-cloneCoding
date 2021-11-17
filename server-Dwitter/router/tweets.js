c990f262cfbfd4e242a19493a0e732f024a0e93f
jhkim9513 <dondon9513@gmail.com>
2021-11-17 18:03:45 +0900

    Among the mvc models, the model was created on the server.

--- server-Dwitter/router/tweets.js
+++ server-Dwitter/router/tweets.js
@@ -1,38 +1,22 @@
 import express from 'express';
 import 'express-async-errors';
+import * as tweetRepository from '../data/tweet.js'
 
 
-let tweets = [
-    {
-        id: '1',
-        text: '드림코딩 화이팅!',
-        createdAt: Date.now().toString(),
-        name: 'Bob',
-        username: 'bob',
-        url: '',
-    },
-    {
-        id: '2',
-        text: '하이',
-        createdAt: Date.now().toString(),
-        name: 'Kim',
-        username: 'kim',
-        url: '',
-    }
-];
 const router = express.Router();
 
 // GET /tweets
 // GET /tweets?username=:username
 router.get('/', (req, res, next) => {
     const username = req.query.username;
-    const data = username ? tweets.filter(t => t.username === username) : tweets;
+    const data = username ? tweetRepository.getAllByUsername(username)
+    : tweetRepository.getAll();
     res.status(200).json(data);
 })
 // GET /tweets/:id
 router.get('/:id', (req, res, next) => {
     const id = req.params.id;
-    const tweet = tweets.find(tweet => tweet.id === id);
+    const tweet = tweetRepository.getById(id);
     if(tweet) {
         res.status(200).json(tweet);
     } else {
@@ -43,21 +27,14 @@ router.get('/:id', (req, res, next) => {
 router.post('/', (req, res, next) => {
     // object destructuring
     const {text, name, username} = req.body;
-    const tweet = {
-        id: Date.now().toString(),
-        text,
-        createdAt: new Date,
-        name,
-        username,
-    };
-    tweets = [tweet, ...tweets];
+    const tweet = tweetRepository.create(text, name, username);
     res.status(201).json(tweet);
 })
 // PUT /tweets/:id
 router.put('/:id', (req, res, next) => {
     const id = req.params.id;
     const text = req.body.text;
-    const tweet = tweets.find(tweet => tweet.id === id);
+    const tweet = tweetRepository.update(id, text);
     if(tweet) {
         tweet.text = text;
         res.status(200).json(tweet);
@@ -68,7 +45,7 @@ router.put('/:id', (req, res, next) => {
 // DELETE /tweets/:id
 router.delete('/:id', (req, res, next) => {
     const id = req.params.id;
-    tweets = tweets.filter(tweet => tweet.id !== id);
+    tweetRepository.remove(id);
     res.sendStatus(204);
 })
 
