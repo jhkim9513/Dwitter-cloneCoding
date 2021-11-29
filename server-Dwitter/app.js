9c87d706e75055c137d4bb717221d68f494c953b
jhkim9513 <dondon9513@gmail.com>
2021-11-29 18:51:35 +0900

    try using mysql about Auth

--- server-Dwitter/app.js
+++ server-Dwitter/app.js
@@ -28,6 +28,7 @@ app.use((error, req, res, next) => {
   res.sendStatus(500);
 });
 
-db.getConnection().then((connection) => console.log(connection));
+// db.getConnection().then((connection) => console.log(connection));
+db.getConnection();
 const server = app.listen(config.host.port);
 initSocket(server);
