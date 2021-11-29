e7861c06669ea34fc9c2bb7de21414527ad0e997
jhkim9513 <dondon9513@gmail.com>
2021-11-29 18:13:12 +0900

    commit before branch creation

--- server-Dwitter/app.js
+++ server-Dwitter/app.js
@@ -1,22 +1,23 @@
-import express from 'express';
-import 'express-async-errors';
-import cors from 'cors';
-import morgan from 'morgan';
-import helmet from 'helmet';
-import tweetsRouter from './router/tweets.js';
-import authRouter from './router/auth.js';
-import { config } from './config.js';
-import { initSocket } from './connection/socket.js';
+import express from "express";
+import "express-async-errors";
+import cors from "cors";
+import morgan from "morgan";
+import helmet from "helmet";
+import tweetsRouter from "./router/tweets.js";
+import authRouter from "./router/auth.js";
+import { config } from "./config.js";
+import { initSocket } from "./connection/socket.js";
+import { db } from "./db/database.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(helmet());
 app.use(cors());
-app.use(morgan('tiny'));
+app.use(morgan("tiny"));
 
-app.use('/tweets', tweetsRouter);
-app.use('/auth', authRouter);
+app.use("/tweets", tweetsRouter);
+app.use("/auth", authRouter);
 
 app.use((req, res, next) => {
   res.sendStatus(404);
@@ -27,5 +28,6 @@ app.use((error, req, res, next) => {
   res.sendStatus(500);
 });
 
+db.getConnection().then((connection) => console.log(connection));
 const server = app.listen(config.host.port);
 initSocket(server);
